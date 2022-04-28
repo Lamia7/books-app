653ee63a639b55bd1b30b244f74eb68a1e500e3d
Ellamia <lamia.elralimi@nova-technology.fr>
2022-04-28 16:22:04 +0200

    feat: afficher livres recuperes par l'API

--- src/containers/SearchBooks.js
+++ src/containers/SearchBooks.js
@@ -23,6 +23,66 @@ const SearchBooks = () => {
         dispatch(fetchBooks(searchSubject))
     }
 
+    const dispayFetchedBooks = state.isLoading ? (
+      <div className="d-flex justif-content-center">
+        <div className="spinner-border text info" role="status">
+          <span className="sr-only"></span>
+        </div>
+      </div>
+    )
+    : state.error !== '' ? (
+      <p>{state.error}</p>
+    )
+    :
+    (
+      state.fetchedBooks.map((data) => {
+        return (
+          <div className='card mb-2' key={data.id}>
+            {/* HEADER */}
+            <div className='card-header'>
+              <h5 className="mb-0">
+                <button 
+                  className="btn btn-link collapsed"
+                  data-toggle="collapse"
+                  data-target={`#${data.id}`}
+                  aria-expanded="false"
+                >
+                  {data.volumeInfo.title}
+                </button>
+              </h5>
+            </div>
+
+            <div id={data.id} className="collapse" data-parent="#accordion">
+                <div className='card-body'>
+                  {
+                    data.volumeInfo.hasOwnProperty('imageLinks') &&
+                    <img 
+                      src={data.volumeInfo.imageLinks.thumbnail}
+                      alt={data.volumeInfo.title}
+                    />
+                  }
+
+                  <br />
+                  {/* CONTENU */}
+                  <h4 className="card-title">Titre: {data.volumeInfo.title}</h4>
+                  <h5 className="card-title">Auteurs: {data.volumeInfo.authors}</h5>
+                  <p className="description">{data.volumeInfo.description}</p>
+                  
+                  {/* LIEN / BOUTON */}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-outline-secondary"
+                    href={data.volumeInfo.previewLink}
+                  >Plus d'infos</a>
+                  <button className="btn btn-outline-secondary ml-3">Enregistrer</button>
+                </div>
+            </div>
+        </div>
+        )
+      })
+    )
+
   return (
     <main role="main">
     <div className="jumbotrom jumbotrom-fluid">
@@ -56,23 +116,8 @@ const SearchBooks = () => {
       </div>
     </div>
     <div className="container" style={{ minHeight: "200px" }}>
-      <div className='accordion'>
-          <div className='card mb-2'>
-            <div className='card-header'>
-                <div className="collapse" data-parent="accordion">
-                    <div className='card-body'>
-                        {/* 
-                        - Image
-                        - Titre
-                        - Auteur
-                        - Description
-                        - Btn plus d'infos
-                        - Brn Enregistrer
-                        */}
-                    </div>
-                </div>
-            </div>
-          </div>
+      <div id="accordion">
+      { dispayFetchedBooks }
       </div>
     </div>
   </main>
